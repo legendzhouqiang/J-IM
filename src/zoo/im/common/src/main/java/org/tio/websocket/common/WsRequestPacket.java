6497f00c373feb218a59842a53d623436f69001e
talent-tan <tywo45@163.com>
2017-08-08 15:04:06 +0800

    no commit message

--- src/zoo/im/common/src/main/java/org/tio/websocket/common/WsRequestPacket.java
+++
@@ -1,9 +0,0 @@
-package org.tio.websocket.common;
-
-/**
- * 
- * @author tanyaowu 
- * 2017年7月30日 上午10:09:46
- */
-public class WsRequestPacket extends WsPacket {
-}
--- src/zoo/im/common/src/main/java/org/tio/websocket/common/WsResponsePacket.java
+++
@@ -1,9 +0,0 @@
-package org.tio.websocket.common;
-
-/**
- * 
- * @author tanyaowu 
- * 2017年7月30日 上午10:09:59
- */
-public class WsResponsePacket extends WsPacket {
-}
