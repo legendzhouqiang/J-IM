584ea3c7f23cfefd61a7b99403a83df27bd5a4e8
heqiao <heqiao2@gome.com.cn>
2017-08-10 21:30:37 +0800

    commit code.

---
+++ src/core-refactor/src/main/java/org/tio/client/ReconnectConfig.java
@@ -0,0 +1,11 @@
+package org.tio.client;
+
+/**
+ * Copyright (c) for darkidiot
+ * Date:2017/8/10
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+public class ReconnConfig {
+}
