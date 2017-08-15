4cfe589c8cc65ecec475d4b7891b419529d805f6
heqiao <heqiao2@gome.com.cn>
2017-08-15 21:15:08 +0800

    commit code.

--- src/core-refactor/src/main/java/org/tio/common/Packet.java
+++ src/core-refactor/src/main/java/org/tio/common/Packet.java
@@ -1,15 +1,34 @@
 package org.tio.common;
 
+import java.io.Serializable;
+
 /**
  * Copyright (c) for darkidiot
  * Date:2017/8/14
  * Author: <a href="darkidiot@icloud.com">darkidiot</a>
  * Desc: 数据包
  */
-public interface Packet {
+public interface Packet extends Serializable {
 
-    void id();
+    short packetCount();
 
-    void byteBuffer();
+    void setPacketCount();
 
+    byte packetType();
+
+    byte setPacketType(byte packetType);
+
+    byte checkSum();
+
+    byte setCheckSum(byte checkSum);
+
+    byte[] header();
+
+    byte optionalLength();
+
+    byte[] optional();
+
+    short bodyLength();
+
+    byte[] body();
 }
---
+++ src/core-refactor/src/main/java/org/tio/common/TioException.java
@@ -0,0 +1,11 @@
+package org.tio.common;
+
+/**
+ * Copyright (c) for darkidiot
+ * Date:2017/8/14
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * Desc: 基础异常类
+ */
+public class TioException extends Exception {
+    private int code;
+}
