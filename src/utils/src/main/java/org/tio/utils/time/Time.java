81a9b1816a34495ed839fae00905fceda634836c
talent-tan <tywo45@163.com>
2017-08-24 09:38:35 +0800

    no commit message

--- src/utils/src/main/java/org/tio/utils/time/Time.java
+++ src/utils/src/main/java/org/tio/utils/time/Time.java
@@ -10,12 +10,22 @@ import org.slf4j.LoggerFactory;
 public class Time {
 	private static Logger log = LoggerFactory.getLogger(Time.class);
 
+	/**
+	 * 一秒
+	 */
 	public static final Long SECOND_1 = 1L;
 
+	/**
+	 * 一分钟
+	 */
 	public static final Long MINUTE_1 = SECOND_1 * 60L;
-
+	/**
+	 * 一小时
+	 */
 	public static final Long HOUR_1 = MINUTE_1 * 60L;
-
+	/**
+	 * 一天
+	 */
 	public static final Long DAY_1 = HOUR_1 * 24;
 
 	/**
