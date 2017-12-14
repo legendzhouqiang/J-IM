54e2362e07752e7c8b834c1cfbe5374cddd82b64
talent-tan <tywo45@163.com>
2017-12-14 18:37:26 +0800

    版本升级到2.0.3.v20171215-RELEASE

--- src/zoo/websocket/server/installAndCopy.bat
+++ src/zoo/websocket/server/installAndCopy.bat
@@ -1,5 +1,5 @@
 call mvn -Dmaven.test.skip=true clean install
 
 call rd ..\..\..\..\dist\examples\im\server /s /q
-call xcopy target\dist\tio-examples-im-server-2.0.2.v20171129-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
+call xcopy target\dist\tio-examples-im-server-2.0.3.v20171215-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
 
