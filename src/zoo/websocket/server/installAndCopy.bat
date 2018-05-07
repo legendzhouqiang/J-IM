926962285b43115c75ed7314130c65169844e823
talent-tan <tywo45@163.com>
2018-05-07 09:55:55 +0800

    发布2.4.0

--- src/zoo/websocket/server/installAndCopy.bat
+++ src/zoo/websocket/server/installAndCopy.bat
@@ -1,5 +1,5 @@
 call mvn -Dmaven.test.skip=true clean install
 
 call rd ..\..\..\..\dist\examples\im\server /s /q
-call xcopy target\dist\tio-examples-im-server-2.3.0.v20180506-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
+call xcopy target\dist\tio-examples-im-server-2.4.0.v20180508-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
 
